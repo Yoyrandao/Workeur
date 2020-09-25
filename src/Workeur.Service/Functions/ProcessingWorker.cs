bdc0735fe136f54b546c1c7b85ab56b54af566d1
Gubanov Ilya <yoyrandao@gmail.com>
2020-09-25 18:13:52 +0300

    added function base

---
+++ src/Workeur.Service/Functions/ProcessingWorker.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+
+using Workeur.Common;
+
+namespace Workeur.Service.Functions
+{
+	public class ProcessingWorker
+	{
+		public ProcessingWorker(IContentProvider provider)
+		{
+			_provider = provider;
+		}
+		
+		[FunctionName("ProcessingWorker")]
+		public async Task RunAsync([TimerTrigger("0/5 * * * * *")] TimerInfo myTimer, ILogger log)
+		{
+			log.LogInformation(_provider.ToString());
+			log.LogInformation($"C# Timer trigger function executed at: {DateTime.UtcNow}");
+		}
+
+		private IContentProvider _provider;
+	}
+}
---
+++ src/Workeur.Service/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+
+using Workeur.Common;
+
+[assembly: FunctionsStartup(typeof(Workeur.Service.Startup))]
+
+namespace Workeur.Service
+{
+	public class Startup : FunctionsStartup
+	{
+		public override void Configure(IFunctionsHostBuilder builder)
+		{
+			builder.Services.AddTransient<IContentProvider, WebClientContentProvider>();
+		}
+	}
+}
